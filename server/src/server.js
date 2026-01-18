7f5736d2a740dc8006f24af4b5d60c2037023aed
Shubhantak Pandey <harshpandey270606@gmail.com>
2026-01-18 14:16:33 +0530

    initial commit

---
+++ server/src/server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import cors from 'cors';
+import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
+import {serve} from 'inngest/express'
+import { inngest } from "./lib/inngest.js";
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+// credentials:true meaning?? => server allows a browser to include cookies on request 
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }))
+
+app.use("/api/inngest", serve({client: inngest, functions}))
+
+app.get("/health", (req, res) => {
+    res.status(200).json({ msg: "api is up and running" });
+})
+
+
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(ENV.PORT, () =>
+            console.log("Server is running on port:", ENV.PORT)
+        )
+    } catch (error) {
+        console.error("💥Error starting the server", error);
+    }
+}
+
+startServer()
